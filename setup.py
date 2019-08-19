ac62e3a911a41ebfc41d07c50254caf58129230d
Kevin Smith <kevinsmith.kis@gmail.com>
2019-08-19 18:02:14 +0200

    setup detailed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='sftp_service',
+   version='1.0',
+   description='A SFTP Servcie tool and package',
+   author='Kevin Smith',
+   author_email='kevinsmith.kis@gmail.com',
+   packages=['sftp_service'],
+   install_requires=[],
+)
